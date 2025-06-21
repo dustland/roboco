880bd1328678922d75893cdd4eca1ec047498f85
GitHub Action <action@github.com>
2025-06-21 09:47:04 +0000

    chore: bump version to 0.10.0

--- src/agentx/__init__.py
+++ src/agentx/__init__.py
@@ -19,7 +19,7 @@ from .tool.models import Tool, tool
 # Logging utilities
 from .utils.logger import setup_clean_chat_logging, set_log_level
 
-__version__ = "0.9.4"
+__version__ = "0.10.0"
 
 __all__ = [
     # Main API - primary entry points
